9a7ddd40efd9f670272bd5d443a1c494d4c962e8
Staff <staff@hiddens-air-5.attlocal.net>
2019-10-02 01:59:30 -0700

    Updated OH Project Working 10/02/19  2:00am

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+  def index
+    @user = { "name" => "dave", "email" => "dave@dave.com" }
+    @user = @user.to_h
+  end
+
+  def make_a_deal
+  end
+  def deal_pg1
+  end
+  def deal_pg2
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'home#index'
+  get '/deals/pg1', to: 'home#deal_pg1'
+  get '/deals/pg2', to: 'home#deal_pg2'
+
+  resources :guess
+    get '/guess_deals1', to: 'guess#deal_pg1'
+    get '/guess_deals2', to: 'guess#deal_pg2'
+    get '/guess_deals3', to: 'guess#deal_pg3'
+    get '/guess_results1', to: 'guess#results_pg1'
+    get '/guess_results2', to: 'guess#results_pg2'
+# render :action => 'show'
+
+
+end
